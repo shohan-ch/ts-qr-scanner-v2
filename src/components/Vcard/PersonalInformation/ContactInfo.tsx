635184e6f6f610870515938ad76be83dcf86adb1
shohan889 <shohan@rootsoftit.com>
2024-03-06 19:41:51 +0600

    docs(personal information) : content value change

--- src/components/Vcard/PersonalInformation/ContactInfo.tsx
+++ src/components/Vcard/PersonalInformation/ContactInfo.tsx
@@ -24,46 +24,36 @@ const ContactInfo = (props: Props) => {
 
   const emailOptions = [
     {
-      name: "type",
-      type: "text",
-      placeHolder: "Write type",
-    },
-    {
       name: "label",
       type: "text",
       placeHolder: "Label...",
     },
     {
-      name: "mobile",
-      type: "password",
-      placeHolder: "Eg.0000 000",
+      name: "email",
+      type: "email",
+      placeHolder: "Eg. name@email.com",
     },
   ];
 
   const websiteOptions = [
     {
-      name: "type",
+      name: "title",
       type: "text",
-      placeHolder: "Write type",
+      placeHolder: "Write here the text URL...",
     },
     {
-      name: "label",
+      name: "website",
       type: "text",
-      placeHolder: "Label...",
-    },
-    {
-      name: "mobile",
-      type: "password",
-      placeHolder: "Eg.0000 000",
+      placeHolder: "https://",
     },
   ];
 
   return (
     <div>
       <h3 className="my-3">Telephone</h3>
-      <BaseMultipleSelect fields={telephoneOptions} title="telephone" />
-      <BaseMultipleSelect fields={emailOptions} title="email" />
-      <BaseMultipleSelect fields={websiteOptions} title="website" />
+      <BaseMultipleSelect fields={telephoneOptions} title="telephones" />
+      <BaseMultipleSelect fields={emailOptions} title="emails" />
+      <BaseMultipleSelect fields={websiteOptions} title="websites" />
     </div>
   );
 };
